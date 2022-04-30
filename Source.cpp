5fdf8ce887804632a1b13bcfd8203bc8cdf54659
SwarnenduRC <103958027+SwarnenduRC@users.noreply.github.com>
2022-05-01 01:58:14 +0530

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,87 @@
+#include "NokiaIPValidator.h"
+
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void processFile(const std::string& fileName)
+{
+    if (fileName.empty())
+        return;
+
+    try
+    {
+        ifstream fileStream;
+        fileStream.open(fileName, ios::ios_base::in);
+        if (fileStream.is_open())
+        {
+            NokiaIPValidator ipValidator;
+            for (string line; getline(fileStream, line);)
+                ipValidator.pushData(line);
+
+            ipValidator.setReadingDataDone(true);
+            fileStream.close();
+            while (!ipValidator.isProcessingDone())
+            {
+                this_thread::sleep_for(chrono::microseconds(1));
+                continue;
+            }
+            cout << "Total IPV4 address count = " << ipValidator.getTotalIPV4AddrCnt() << endl;
+            cout << "Total IPV6 address count = " << ipValidator.getTotalIPV6AddrCnt() << endl;
+            cout << "Unique IPV4 address count = " << ipValidator.getUniqueIPV4AddrCnt() << endl;
+            cout << "Unique IPV6 address count = " << ipValidator.getUniqueIPV6AddrCnt() << endl;
+            cout << "Invalid IP address count = " << ipValidator.getInvalidIPAddrCnt() << endl;
+        }
+    }
+    catch (const exception& excp)
+    {
+        cerr << "Exception caught: " << excp.what() << endl;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    cout << endl;
+    if (argc < 2)
+    {
+        cerr << "No file provided to process. Returning...." << endl;
+    }
+    else if (argc > 3)
+    {
+        cerr << "Too many parameters. Not processing..." << endl;
+    }
+    else
+    {
+        if (argc == 2)
+        {
+            string fileName(argv[1]);
+            processFile(fileName);
+        }
+        else
+        {
+            string fileName(argv[1]);
+            string filePath;
+
+#if defined(_WIN64) || defined(_WIN32)
+            filePath = argv[2];
+            if (filePath[filePath.size() - 1] == '\'')
+                fileName = filePath + fileName;
+            else
+                fileName = filePath + "\"" + fileName;
+#else
+            filePath = argv[2];
+            if (filePath[filePath.size() - 1] == '/')
+                fileName = filePath + fileName;
+            else
+                fileName = filePath + "/" + fileName;
+
+#endif // _WIN64 || _WIN32
+
+        }
+    }
+    return 0;
+}
